411c126373328677a1dbaecff5d2a67e7d8a73c0
Matere49 <mta1432@gmail.com>
2022-08-08 20:38:27 -0500

    simplified code

--- src/app.js
+++ src/app.js
@@ -32,38 +32,13 @@ let months = [
   "December",
 ];
 let month = months[now.getMonth()];
+
 h3.innerHTML = `${day} ${month} ${date}, ${hours}:${minutes}`;
 
-function displayForecast() {
-  let forecastElement = document.querySelector("#forecast");
-
-  let forecastHTML = `<div class="row">`;
-  let days = ["Mon", "Tues", "Wed", "Thurs", "Fri"];
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `
-          <div class="col-2">
-            <div class="forecast-date">
-              ${day}
-            </div>
-            <img src="http://openweathermap.org/img/wn/10d@2x.png" alt="" width="50">
-            <div class="forecast-temp">
-              <span class="weather-forecast-temp-max">25°</span>
-              <span class="weather-forecast-temp-min">18°</span>
-            </div>
-          </div>`;
-  });
-
-  forecastHTML = forecastHTML + `</div>`;
-  forecastElement.innerHTML = forecastHTML;
-}
-
-//Search engine
 function showWeather(response) {
   let temperatureElement = document.querySelector("#temp");
   celsiusTemperature = response.data.main.temp;
-  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+  temperatureElement.innerHTML = Math.round(celsiusTemperature) + `°`;
 
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.name;
@@ -72,7 +47,7 @@ function showWeather(response) {
   descriptionElement.innerHTML = response.data.weather[0].description;
 
   let humidityElement = document.querySelector("#humidity");
-  humidityElement.innerHTML = response.data.main.humidity;
+  humidityElement.innerHTML = response.data.main.humidity + `%`;
 
   let windElement = document.querySelector("#wind");
   windElement.innerHTML = Math.round(response.data.wind.speed);
@@ -88,24 +63,25 @@ function showWeather(response) {
 function findCity(city) {
   let apiKey = "81600396eecd586cc7169c8c188c8cf4";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
   axios.get(apiUrl).then(showWeather);
 }
 
 function handleSubmit(event) {
   event.preventDefault();
-  let city = document.querySelector("#city-search").value;
-  findCity(city);
+  let city = document.querySelector("#city-search");
+  findCity(city.value);
+}
+
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
 }
 
 function searchLocation(position) {
   let apiKey = "81600396eecd586cc7169c8c188c8cf4";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(showWeather);
-}
-
-function getCurrentLocation(event) {
-  event.preventDefault();
-  navigator.geolocation.getCurrentPosition(searchLocation);
 }
 
 let searchForm = document.querySelector("#search-form");
@@ -115,4 +91,3 @@ let currentLocationButton = document.querySelector("#current-location-button");
 currentLocationButton.addEventListener("click", getCurrentLocation);
 
 findCity("Fort Worth");
-displayForecast();
