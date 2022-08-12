f9992c61ebae690acf25b1fe7067b77e33948e77
Matere49 <mta1432@gmail.com>
2022-08-12 12:54:27 -0500

    Fixed current location button

--- src/app.js
+++ src/app.js
@@ -128,6 +128,14 @@ function searchLocation(position) {
   axios.get(apiUrl).then(showWeather);
 }
 
+function currentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+let currentLocationButton = document.querySelector("#current-location-button");
+currentLocationButton.addEventListener("click", currentLocation);
+
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", handleSubmit);
 
